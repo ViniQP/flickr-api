db3b6de121da8ce6d0f3660a06c095b3bf30d54a
Vinícius M <viniqpoweryt@gmail.com>
2023-03-01 14:32:01 -0300

    Add galleries to index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,19 @@
 class StaticPagesController < ApplicationController
   require 'flickr'
+  require 'json'
 
   def index
     @flickr = Flickr.new
-    @photos = @flickr.people.getPhotos(user_id: "197852001@N08")
+
+    unless params[:user_id].blank?
+      @photos = @flickr.people.getPhotos(user_id: params[:user_id], per_page: 10)
+      @info = @flickr.people.getInfo(user_id: @photos.first.owner)
+    else
+      @photos = @flickr.galleries.getPhotos(gallery_id: "72157720878919558")
+    end
+  end
+
+  def get_userid
+    params.permit(:user_id)
   end
 end
